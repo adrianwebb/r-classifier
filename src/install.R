72e15d0e7f627f049a1388fc3add30789b7fc16e
Adrian Webb <adrian.webb@gsa.gov>
2016-02-13 14:03:58 -0500

    Adding initial install, init, and clean R scripts.

---
+++ src/install.R
@@ -0,0 +1,22 @@
+
+#-------------------------------------------------------------------------------
+# Install dependencies
+
+install.packages(c(
+  'ProjectTemplate',
+  'log4r',
+  'reshape',
+  'plyr',
+  'ggplot2',
+  'stringr',
+  'lubridate',
+  'data.table',
+  'debug',
+  'gtools',
+  'digest',
+  'quantmod',
+  'PerformanceAnalytics',
+  'RColorBrewer',
+  'foreach',
+  'doParallel'
+))
